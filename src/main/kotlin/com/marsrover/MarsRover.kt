95b0959ddbb8516fbeafa66c7ac200204366de8f
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-06-25 15:41:03 +0200

    refactor would go outside method

--- src/main/kotlin/com/marsrover/MarsRover.kt
+++ src/main/kotlin/com/marsrover/MarsRover.kt
@@ -1,21 +1,18 @@
 package com.marsrover
 
-import com.marsrover.Direction.Companion.DOWN
-import com.marsrover.Direction.Companion.UP
-
 class MarsRover(private val rover: Rover) {
+
     fun move(direction: Direction) {
         if (wouldGoOutside(direction)) return
         rover.move(direction)
     }
 
     private fun wouldGoOutside(direction: Direction): Boolean {
-        if (direction == UP && rover.position.row >= 9) {
-            return true
-        } else if (direction == DOWN && rover.position.row <= 0) {
-            return true
+        if(rover.position.row + direction.rowIncrement in 9 downTo 0) {
+            return false
         }
-        return false
+        return true
     }
+
 
 }

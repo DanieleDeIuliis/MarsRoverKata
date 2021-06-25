963e21ae9543c431e02d070c0585946d1a3b7fd5
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-06-25 16:07:19 +0200

    used enum instead of direction class to model the direction

--- src/main/kotlin/com/marsrover/Direction.kt
+++ src/main/kotlin/com/marsrover/Direction.kt
@@ -1,10 +1,9 @@
 package com.marsrover
 
-data class Direction(val rowIncrement: Int, val columnIncrement: Int) {
-    companion object {
-        val UP = Direction(1, 0)
-        val DOWN: Direction = Direction(-1, 0)
-        val EAST: Direction = Direction(0, 1)
-        val WEST: Direction = Direction(0, -1)
-    }
+enum class Direction(val rowIncrement: Int, val columnIncrement: Int) {
+
+    N(1,0),
+    S(-1,0),
+    E(0,1),
+    W(0,-1)
 }
--- src/test/kotlin/com/marsrover/MarsRoverTest.kt
+++ src/test/kotlin/com/marsrover/MarsRoverTest.kt
@@ -1,90 +1,87 @@
 package com.marsrover
 
-import com.marsrover.Direction.Companion.DOWN
-import com.marsrover.Direction.Companion.EAST
-import com.marsrover.Direction.Companion.UP
-import com.marsrover.Direction.Companion.WEST
+import com.marsrover.Direction.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class MarsRoverTest {
     @Test
     fun `move a rover up`() {
-        val rover = Rover(position = Position(row = 0, column = 0), direction = UP)
+        val rover = Rover(position = Position(row = 0, column = 0), direction = N)
 
-        MarsRover(rover).move(UP)
+        MarsRover(rover).move(N)
 
-        assertThat(rover.direction).isEqualTo(UP)
+        assertThat(rover.direction).isEqualTo(N)
         assertThat(rover.position).isEqualTo(Position(row = 1, column = 0))
     }
 
     @Test
     fun `a rover can't move up if the landing position is outside the grid`() {
-        val rover = Rover(position = Position(row = 9, column = 0), direction = UP)
+        val rover = Rover(position = Position(row = 9, column = 0), direction = N)
 
-        MarsRover(rover).move(UP)
+        MarsRover(rover).move(N)
 
-        assertThat(rover.direction).isEqualTo(UP)
+        assertThat(rover.direction).isEqualTo(N)
         assertThat(rover.position).isEqualTo(Position(row = 9, column = 0))
     }
 
     @Test
     fun `move a rover down`() {
-        val rover = Rover(position = Position(row = 9, column = 0), direction = DOWN)
+        val rover = Rover(position = Position(row = 9, column = 0), direction = S)
 
-        MarsRover(rover).move(DOWN)
+        MarsRover(rover).move(S)
 
-        assertThat(rover.direction).isEqualTo(DOWN)
+        assertThat(rover.direction).isEqualTo(S)
         assertThat(rover.position).isEqualTo(Position(row = 8, column = 0))
     }
 
     @Test
     fun `a rover can't move down if the landing position is outside the grid`() {
-        val rover = Rover(position = Position(row = 0, column = 0), direction = DOWN)
+        val rover = Rover(position = Position(row = 0, column = 0), direction = S)
 
-        MarsRover(rover).move(DOWN)
+        MarsRover(rover).move(S)
 
-        assertThat(rover.direction).isEqualTo(DOWN)
+        assertThat(rover.direction).isEqualTo(S)
         assertThat(rover.position).isEqualTo(Position(row = 0, column = 0))
     }
 
     @Test
     fun `move a rover east`() {
-        val rover = Rover(position = Position(row = 0, column = 0), direction = EAST)
+        val rover = Rover(position = Position(row = 0, column = 0), direction = E)
 
-        MarsRover(rover).move(EAST)
+        MarsRover(rover).move(E)
 
-        assertThat(rover.direction).isEqualTo(EAST)
+        assertThat(rover.direction).isEqualTo(E)
         assertThat(rover.position).isEqualTo(Position(row = 0, column = 1))
     }
 
     @Test
     fun `a rover can't move east if the landing position is outside the grid`() {
-        val rover = Rover(position = Position(row = 0, column = 9), direction = EAST)
+        val rover = Rover(position = Position(row = 0, column = 9), direction = E)
 
-        MarsRover(rover).move(EAST)
+        MarsRover(rover).move(E)
 
-        assertThat(rover.direction).isEqualTo(EAST)
+        assertThat(rover.direction).isEqualTo(E)
         assertThat(rover.position).isEqualTo(Position(row = 0, column = 9))
     }
 
     @Test
     fun `move a rover west`() {
-        val rover = Rover(position = Position(row = 0, column = 9), direction = WEST)
+        val rover = Rover(position = Position(row = 0, column = 9), direction = W)
 
-        MarsRover(rover).move(WEST)
+        MarsRover(rover).move(W)
 
-        assertThat(rover.direction).isEqualTo(WEST)
+        assertThat(rover.direction).isEqualTo(W)
         assertThat(rover.position).isEqualTo(Position(row = 0, column = 8))
     }
 
     @Test
     fun `a rover can't move west if the landing position is outside the grid`() {
-        val rover = Rover(position = Position(row = 0, column = 0), direction = WEST)
+        val rover = Rover(position = Position(row = 0, column = 0), direction = W)
 
-        MarsRover(rover).move(WEST)
+        MarsRover(rover).move(W)
 
-        assertThat(rover.direction).isEqualTo(WEST)
+        assertThat(rover.direction).isEqualTo(W)
         assertThat(rover.position).isEqualTo(Position(row = 0, column = 0))
     }
 }

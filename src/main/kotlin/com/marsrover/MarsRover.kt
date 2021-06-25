95d414d6c611769a28262bbd0b9e512c2bfd36b8
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-06-25 15:14:03 +0200

    a rover can't move up outside the borders

--- src/main/kotlin/com/marsrover/MarsRover.kt
+++ src/main/kotlin/com/marsrover/MarsRover.kt
@@ -4,6 +4,9 @@ import com.marsrover.Direction.Companion.UP
 
 class MarsRover(private val rover: Rover) {
     fun move(direction: Direction) {
+        if(direction == UP && rover.position.row >= 9) {
+            return
+        }
         rover.direction = direction
         if(direction == UP) {
             rover.position = Position(rover.position.row + 1, rover.position.column)
--- src/test/kotlin/com/marsrover/MarsRoverTest.kt
+++ src/test/kotlin/com/marsrover/MarsRoverTest.kt
@@ -13,5 +13,15 @@ class MarsRoverTest {
         assertThat(rover.direction).isEqualTo(Direction.UP)
         assertThat(rover.position).isEqualTo(Position(row = 1, column = 0))
     }
+
+    @Test
+    fun `a rover can't move up if the landing position is outside the grid`() {
+        val rover = Rover(position = Position(row = 9, column = 0), direction = Direction.UP)
+
+        MarsRover(rover).move(Direction.UP)
+
+        assertThat(rover.direction).isEqualTo(Direction.UP)
+        assertThat(rover.position).isEqualTo(Position(row = 9, column = 0))
+    }
 }
 

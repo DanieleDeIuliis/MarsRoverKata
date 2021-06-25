c71fd74852d904d4422107e68f08623c046eab82
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-06-25 15:20:29 +0200

    extracted a check next position method

--- src/main/kotlin/com/marsrover/MarsRover.kt
+++ src/main/kotlin/com/marsrover/MarsRover.kt
@@ -4,11 +4,15 @@ import com.marsrover.Direction.Companion.UP
 
 class MarsRover(private val rover: Rover) {
     fun move(direction: Direction) {
-        if(direction == UP && rover.position.row >= 9) {
-            return
+        if (wouldGoOutside(direction)) return
+        rover.move(direction)
+    }
+
+    private fun wouldGoOutside(direction: Direction): Boolean {
+        if (direction == UP && rover.position.row >= 9) {
+            return true
         }
-        val aRover = rover
-        aRover.move(direction)
+        return false
     }
 
 }

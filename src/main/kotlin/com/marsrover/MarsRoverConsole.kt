73d28aa68c02a5d6585c6ac7446677befa62e017
Daniele De Iuliis <daniele.deiuliis@gmail.com>
2021-06-29 15:11:48 +0200

    refactor

--- src/main/kotlin/com/marsrover/MarsRoverConsole.kt
+++ src/main/kotlin/com/marsrover/MarsRoverConsole.kt
@@ -2,23 +2,32 @@ package com.marsrover
 
 import com.marsrover.Direction.*
 import com.marsrover.TurnDirection.*
+
 class MarsRoverConsole(private val ioStream: IOStream, private val marsRover: MarsRover) {
+
     fun moveRoverOnMars() {
-        val input = ioStream.readInput()
         var outputPrefix = ""
-        input.forEach { command ->
-            when (command) {
-                'M' -> try {
-                        marsRover.move()
-                    } catch (e: ObstacleInPositionException) {
-                    outputPrefix = "0:"
-                    }
-                'L' -> marsRover.turn(LEFT)
-                'R' -> marsRover.turn(RIGHT)
-            }
+        try {
+            executeCommands()
+        } catch (e: ObstacleInPositionException) {
+            outputPrefix = "0:"
         }
-        val output =  outputPrefix + marsRover.formattedStateString()
+        val output = outputPrefix + marsRover.formattedStateString()
         ioStream.writeOutput(output)
+    }
+
+    private fun executeCommands() {
+        ioStream.readInput().forEach { command ->
+            execute(command)
+        }
+    }
+
+    private fun execute(command: Char) {
+        when (command) {
+            'M' -> marsRover.move()
+            'L' -> marsRover.turn(LEFT)
+            'R' -> marsRover.turn(RIGHT)
+        }
     }
 
     private fun MarsRover.formattedStateString(): String {
